cd719c81d65a5248146b8f687cb163e493cec919
Xavier2323 <98024928+Xavier2323@users.noreply.github.com>
2024-01-08 21:38:21 +0800

    Update player.h
    fixed

--- TowerGame/player.h
+++ TowerGame/player.h
@@ -20,52 +20,52 @@ public:
     //void heart_draw();
     //void score_draw();
     int player_go_die();
-    int x_val(){
+    int get_x(){
         return x;
     }
-    int y_val(){
+    int get_y(){
         return y;
     }
-    int ox_val(){
+    int get_ox(){
         return ox;
     }
-    int oy_val(){
+    int get_oy(){
         return oy;
     }
-    void x_eval(int u){
+    void set_x(int u){
         x=u;
     }
-    void y_eval(int u){
+    void set_y(int u){
         y=u;
     }
-    void ox_eval(int u){
+    void set_ox(int u){
         ox=u;
     }
-    void oy_eval(int u){
+    void set_oy(int u){
         oy=u;
     }
-    int HP_val() {
+    int get_HP() {
         return HP;
     }
-    void HP_eval(int u){
+    void set_HP(int u){
         HP=u;
     }
-    int dir_val() {
+    int get_dir() {
         return dir;
     }
-    void dir_eval(int u){
+    void set_dir(int u){
         dir=u;
     }
-    int state_val() {
+    int get_state() {
         return state;
     }
-    void state_eval(int u){
+    void set_state(int u){
         state=u;
     }
-    int frame_val() {
+    int get_frame() {
         return frame;
     }
-    void frame_eval(int u){
+    void set_frame(int u){
         frame=u;
     }
 private:
